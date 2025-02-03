180b032ed8d53deeffedea06b7f644e792f1b720
이지혜 <59763645+dev-jihye@users.noreply.github.com>
2025-02-03 16:31:52 +0900

    🗃️ 데이터베이스 연동 및 정적 이미지 추가

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -7,6 +9,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   // Set headers to prevent CORS errors
@@ -21,9 +24,16 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
 mongoose
   .connect(
-    'mongodb+srv://jihye:oUt4wRjETTK2jz3t@cluster0.n3omu.mongodb.net/shop'
+    'mongodb+srv://jihye:oUt4wRjETTK2jz3t@cluster0.n3omu.mongodb.net/messages?retryWrites=true'
   )
   .then((result) => {
     app.listen(8080);
